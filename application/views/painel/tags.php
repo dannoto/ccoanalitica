9c1d7609401a7776dd4741d508be920131a92d8c
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-12-20 13:54:39 -0300

    upload para deploy

--- application/views/painel/tags.php
+++ application/views/painel/tags.php
@@ -155,7 +155,7 @@
                         <td>${tag.tag_slug}</td>
                         <td>${tag.tag_description || ''}</td>
                         <td class="actions">
-                        <a href="${base_url()}modelos/${tag.tag_slug}">
+                        <a href="https://ccoanalitica.com/modelos/${tag.tag_slug}">
                              <button>
                                 <i style="color:#FFF" class="fa fa-eyes">VER</i>
                             </button>
