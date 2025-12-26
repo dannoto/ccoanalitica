b6b5e9c24ecb46ba7541855a4201c188cbc392e7
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-12-26 18:49:18 -0300

    modelos/odontologia/um

--- application/views/modelos/um/index.php
+++ application/views/modelos/um/index.php
@@ -557,10 +557,10 @@
                                 <div class="accordion-section">
                                     <?php foreach ($d['faq']['items'] as $key => $item): ?>
                                         <div class="accordion-section-title" data-tab="#accordion-a<?= $key ?>?>">
-                                            <?= $item['item']['pergunta']?>
+                                            <?= $item['pergunta']?>
                                         </div>
                                         <div class="accordion-section-content" id="accordion-a<?= $key ?>?>">
-                                            <?= $item['item']['resposta']?>
+                                            <?= $item['resposta']?>
                                         </div>
                                     
                                     <?php endforeach; ?>
