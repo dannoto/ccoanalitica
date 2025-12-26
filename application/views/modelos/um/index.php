8c26f14ff473c09a8d4e5e16d71c7141b7971c78
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-12-26 17:58:02 -0300

    modelos/odontologia/um

--- application/views/modelos/um/index.php
+++ application/views/modelos/um/index.php
@@ -383,145 +383,145 @@
                   <div id="gallery" class="row g-3 wow fadeIn" data-wow-delay=".3s">
 
                     <div class="col-md-3 col-sm-6 col-12 item facilities">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l1.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l1.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l1.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l1.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item facilities">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l2.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l2.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l2.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l2.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item facilities">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l3.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l3.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l3.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l3.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item facilities">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l4.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l4.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l4.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l4.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item facilities">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l5.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l5.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l5.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l5.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item dentists">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l6.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l6.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l6.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l6.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item dentists">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l7.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l7.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l7.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l7.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item dentists">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l8.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l8.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l8.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l8.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item services">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l9.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l9.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l9.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l9.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item services">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l10.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l10.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l10.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l10.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item services">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l11.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l11.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l11.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l11.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
 
                     <div class="col-md-3 col-sm-6 col-12 item services">
-                        <a href="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l12.webp" class="image-popup d-block hover">
+                        <a href="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l12.webp" class="image-popup d-block hover">
                             <div class="relative overflow-hidden rounded-1">
                                 <div class="absolute start-0 w-100 hover-op-1 p-5 abs-middle z-2 text-center text-white z-3">
                                     View
                                 </div>
                                 <div class="absolute start-0 w-100 h-100 overlay-black-5 hover-op-1 z-2"></div>
-                                <img src="<?= base_url() ?>?>dist/modelos/um/assets/images/gallery/l12.webp" class="w-100 hover-scale-1-2" alt="">
+                                <img src="<?= base_url() ?>dist/modelos/um/assets/images/gallery/l12.webp" class="w-100 hover-scale-1-2" alt="">
                             </div>
                         </a>
                     </div>
