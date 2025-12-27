03964a248a3e988278eba484edaffaa2dfe8fb8a
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-12-26 21:26:32 -0300

    modelos/odontologia/um

--- application/views/modelos/um/index.php
+++ application/views/modelos/um/index.php
@@ -560,8 +560,7 @@
 
                         </div>
                     </div>
-
-                    <div class="col-lg-4 col-sm-12 order-lg-1 order-sm-2">
+                    <div class="col-lg-2 col-sm-12 order-lg-1 order-sm-2">
                         <div class="row">
                             <div class="col-lg-6 col-sm-6">
                                 <div class="widget">
@@ -577,7 +576,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-sm-12 order-lg-1 order-sm-2">
+                    <div class="col-lg-2 col-sm-12 order-lg-1 order-sm-2">
                         <div class="row">
                             <div class="col-lg-6 col-sm-6">
                                 <div class="widget">
