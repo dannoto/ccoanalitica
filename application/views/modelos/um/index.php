09e5e45a88fa8b10c770ff9194998861d225de77
Daniel Ribeiro <sdanrib2018@gmail.com>
2025-12-26 22:06:07 -0300

    modelos/odontologia/um

--- application/views/modelos/um/index.php
+++ application/views/modelos/um/index.php
@@ -578,7 +578,7 @@
                             </a>
 
                             <a href="<?= $d['contato']['social']['x'] ?>">
-                                <svg width="39" height="39" id="fa6-brands--square-x-twitter" viewBox="0 0 448 512">
+                                <svg width="38" height="38" id="fa6-brands--square-x-twitter" viewBox="0 0 448 512">
                                     <path fill="#FFFFFF" d="M64 32C28.7 32 0 60.7 0 96v320c0 35.3 28.7 64 64 64h320c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64zm297.1 84L257.3 234.6L379.4 396h-95.6L209 298.1L123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5l78.2-89.5zm-37.8 251.6L153.4 142.9h-28.3l171.8 224.7h26.3z" />
                                 </svg>
                             </a>
@@ -640,10 +640,17 @@
             <div class="subfooter">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-6 col-12">
                             <div class="de-flex">
                                 <div class="de-flex-col">
                                     <?= $d['footer']['copyright'] ?>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-12">
+                            <div class="de-flex">
+                                <div class="de-flex-col">
+                                    Feito por <a href="https://ccoanalitica.com">CCO ANALÍTICA</a>
                                 </div>
                             </div>
                         </div>
