8c6d1e96b7c0d4536b57f935f6ffc5897025d766
Daniel Ribeiro <sdanrib2018@gmail.com>
2026-01-06 17:49:19 -0300

    .

--- application/views/modelos/quinze/index.php
+++ application/views/modelos/quinze/index.php
@@ -8,15 +8,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <meta name="description" content="Medisch Responsive Bootstrap 4 Medical HTML5 Template"/>
     <title>Home Main - Medisch Responsive Bootstrap 4 Medical HTML5 Template</title>
-    <link href="assets/images/favicon/favicon.png" rel="icon"/>
+    <link href="https://demo.zytheme.com/medisch/assets/images/favicon/favicon.png" rel="icon"/>
     <!--  Fonts ==
     -->
     <link rel="preconnect" href="https://fonts.gstatic.com"/>
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&amp;family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet"/>
     <!--  Stylesheets==
     -->
-    <link href="assets/css/vendor.min.css" rel="stylesheet"/>
-    <link href="assets/css/style.css" rel="stylesheet"/>
+    <link href="https://demo.zytheme.com/medisch/assets/css/vendor.min.css" rel="stylesheet"/>
+    <link href="https://demo.zytheme.com/medisch/assets/css/style.css" rel="stylesheet"/>
     <!-- Google Tag Manager-->
     <script>
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
